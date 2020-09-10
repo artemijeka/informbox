2082daa5de4889dd220f1686ba98f581af9e60df
artem <work.kuznecov.samara@yandex.ru>
2020-09-10 10:31:30 +0400

    upd

--- src/js/footer/script.js
+++ src/js/footer/script.js
@@ -1,73 +1,82 @@
 $(function() {
 
 
+
   let jsonUrl = 'https://reqres.in/api/unknown?per_page=12';
   let jsonData = {};
   let resultTableHtml = '';
   
 
-  //вывод данных из json в таблицу
-  $.get(jsonUrl, function(data) {
-    jsonData = data.data;
-    for (var item of jsonData) {
-      let nameCapitalized = item.name.charAt(0).toUpperCase() + item.name.slice(1)
-      resultTableHtml += '<tr><td class="item-id">'+item.id+'</td><td class="item-name">'+nameCapitalized+'</td><td class="item-year">'+item.year+'</td><td class="item-color"><span style="background-color:'+item.color+'"></span>'+item.color+'</td><td class="item-value">'+item.pantone_value+'</td></tr>';
-    }
-    $('#jsTableTbody').html(resultTableHtml);
-  },'json');
+  $(window).on('load', function() {
+    //вывод данных из json в таблицу
+    $.get(jsonUrl, function(data) {
+      jsonData = data.data;
+      for (var item of jsonData) {
+        let nameCapitalized = item.name.charAt(0).toUpperCase() + item.name.slice(1)
+        resultTableHtml += '<tr>';
+
+        if ( Cookies.get('item-id') !== 'hidden') {
+          resultTableHtml += '<td class="item-id">'+item.id+'</td>';
+        } else {
+          $('th.item-id').remove();
+          $('#jsBtnReset').addClass('acts');
+        }
+        if ( Cookies.get('item-name') !== 'hidden') {
+          resultTableHtml += '<td class="item-name">'+nameCapitalized+'</td>';
+        } else {
+          $('th.item-name').remove();
+          $('#jsBtnReset').addClass('acts');
+        }
+        if ( Cookies.get('item-year') !== 'hidden') {
+          resultTableHtml += '<td class="item-year">'+item.year+'</td>';
+        } else {
+          $('th.item-year').remove();
+          $('#jsBtnReset').addClass('acts');
+        }
+        if ( Cookies.get('item-color') !== 'hidden') {
+          resultTableHtml += '<td class="item-color"><span style="background-color:'+item.color+'"></span>'+item.color+'</td>';
+        } else {
+          $('th.item-color').remove();
+          $('#jsBtnReset').addClass('acts');
+        }
+        if ( Cookies.get('item-value') !== 'hidden') {
+          resultTableHtml += '<td class="item-value">'+item.pantone_value+'</td>';
+        } else {
+          $('th.item-value').remove();
+          $('#jsBtnReset').addClass('acts');
+        }
+
+        resultTableHtml += '</tr>';
+      }
+      $('#jsTableTbody').html(resultTableHtml);
+    },'json');
+  });
+
 
 
   //скрытие столбцов при клике на checkbox
   $('.js-checkbox').on('click', function() {
     if ( !$(this)[0].checked ) { //если снят checkbox      
       var targetClass = $(this).data('target');
-      $('.'+targetClass).addClass('hidden');
+      $('.'+targetClass).remove();
       $('#jsBtnReset').addClass('acts');
       Cookies.set(targetClass, 'hidden');
     }
-
   });
 
 
 
-  /* 
-  * item-id
-  * item-name
-  * item-year
-  * item-color
-  * item-value
-  */
-  Cookies.get();
-
-  hideColByCoockie('item-id');
-  hideColByCoockie('item-name');
-  hideColByCoockie('item-year');
-  hideColByCoockie('item-color');
-  hideColByCoockie('item-value');
-  
-  /* TODO */
-  function hideColByCoockie(classOfCol) {
-    console.log(classOfCol);
-    if ( Cookies.get(classOfCol) == 'hidden' ) {
-      console.log( Cookies.get(classOfCol) );
-      $('.' + classOfCol).addClass('hidden');
-      console.log( $('.' + classOfCol) );
-      $('#jsBtnReset').addClass('acts');
-    }
-  }
-
-
-
+  //reset кнопки механизм
   $('#jsBtnReset').on('click', function() {
-    $('.hidden .js-checkbox').click();
-    $('thead tr th, tbody tr td').removeClass('hidden');
+    $('#jsBtnReset').removeClass('acts');    
     Cookies.set('item-id', '');
     Cookies.set('item-name', '');
     Cookies.set('item-year', '');
     Cookies.set('item-color', '');
     Cookies.set('item-value', '');
+    location.reload();
   });
 
-  
+
 
 });
